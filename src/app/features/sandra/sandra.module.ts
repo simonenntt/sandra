0a4415b0abd15d5383eac7bb912812fb187152c0
Simone Iannotti <simonenntt@gmail.com>
2022-06-29 22:46:33 +0200

    sandra component

--- src/app/features/sandra/sandra.module.ts
+++ src/app/features/sandra/sandra.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SandraComponent } from './sandra.component';
+import { RouterModule, Routes } from '@angular/router';
 
-
+const routes: Routes = [
+  {path:'', component:SandraComponent}
+]
 
 @NgModule({
   declarations: [
     SandraComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class SandraModule { }
